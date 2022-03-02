f8faca958c8a1431f9d3eadbac8aa3cc41359010
Peter Meglis <petermeglis@github.com>
2022-03-02 09:45:32 -0700

    Create a dir and file for the tweet

--- main.rb
+++ main.rb
@@ -13,7 +13,15 @@ conn = Faraday.new(
 end
 
 tweet_id = 123
+response = conn.get('tweets', { "expansions": "author_id", "user.fields": "name", "ids": tweet_id })
+tweet_content = response.body['data'][0]['text']
 
-response = conn.get('tweets', ids: tweet_id)
+timestamp = Time.now.strftime("%F %T")
+tweet_author = response.body['includes']['users'][0]['name']
+tweet_title = tweet_content[0..50].gsub!(/[^0-9A-Za-z\s]/, '')
+file_title = "#{timestamp} - #{tweet_author} - #{tweet_title}"
 
-puts response
+directory_name = "./tweets"
+Dir.mkdir(directory_name) unless Dir.exist?(directory_name)
+
+File.open("#{directory_name}/#{file_title}", "w") { |f| f.write tweet_content }
